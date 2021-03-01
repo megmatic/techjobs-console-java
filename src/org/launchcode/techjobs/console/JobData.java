ae76073197ea12101d6ab665def145521d75a921
Meg <meggoodwin57@gmail.com>
2021-03-01 09:50:22 -0600

    added case insensitivity to findByColumnAndValue method so that search by skill with the terms 'javascript' and 'JAVASCRIPT' both return all jobs that contain 'JavaScript'

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by LaunchCode
@@ -76,7 +77,7 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
         }
